44c0fc9482b8747007a915ce77db965d355ab0c6
escapeBaek <escapebaek@gmail.com>
2024-08-28 10:44:42 +0900

    accrac add

--- land/views.py
+++ land/views.py
@@ -15,7 +15,7 @@ def home(request):
         {"title": "OrphanAnesthesia", "text": "Anesthesia care for rare diseases.", "url": "https://www.orphananesthesia.eu/en/rare-diseases/published-guidelines.html"},
         {"title": "Virtual TEE", "text": "Virtual TEE for education.", "url": "https://pie.med.utoronto.ca/TEE/TEE_content/TEE_standardViews_intro.html"},
         {"title": "Virtual FOB", "text": "Virtual FOB for education.", "url": "https://pie.med.utoronto.ca/VB/VB_content/simulation.html"},
+        {"title": "ACCRAC", "text": "Podcast for board examination.", "url": "https://accrac.com/"},
         {"title": "Notion test 1", "text": "Testing for notion link.", "url": "https://escapebaek.notion.site/Brachial-Plexus-Block-1bc4d01a2aa648f09917278463b89ce2?pvs=25"},
-        {"title": "Notion test 2", "text": "Testing for notion link.", "url": "https://escapebaek.notion.site/ERAS-PONV-Guideline-92a68b5dbb85474a85eeee278b636bc7?pvs=4"},
     ]
     return render(request, 'land/home.html', {'cards': cards})
