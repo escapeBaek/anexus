dc0952a7698a87d34569118aff8ae20413af7e50
escapeBaek <escapebaek@gmail.com>
2024-09-14 21:07:51 +0900

    roll back2

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -13,3 +13,16 @@ def user_is_approved(function=None):
     if function:
         return actual_decorator(function)
     return actual_decorator
+
+def user_is_specially_approved(function=None):
+    """
+    Decorator for views that checks that the logged-in user is specially approved.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated and u.is_specially_approved,
+        login_url='login',
+        redirect_field_name=None
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
