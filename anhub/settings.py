c0cef792002b2384a438ad7909f7e4d7270c4d92
escapeBaek <escapebaek@gmail.com>
2024-09-21 15:31:02 +0900

    deploy_host update

--- anhub/settings.py
+++ anhub/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'www.anexus.site', 'anexus.site', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
